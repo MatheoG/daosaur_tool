954c2f83bc26cb0de78bf15103c72cfc5184b329
MatheoG <matheo.guegan@hotmail.fr>
2023-01-29 20:40:05 +0100

    feat: get more data

--- build/opensea.js
+++ build/opensea.js
@@ -17,45 +17,53 @@ const axios_1 = __importDefault(require("axios"));
 const dotenv_1 = require("dotenv");
 const _1 = require(".");
 const fs_1 = __importDefault(require("fs"));
+function getAdresseAsset(address) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const openseaUrl = 'https://api.opensea.io/api/v1/assets';
+        let assets = [];
+        let cursor = '';
+        let error = false;
+        //on boucle jusqu'a ce qu'il n'y ait plus d'erreur
+        do {
+            axios_1.default.get(openseaUrl, {
+                headers: {
+                    'X-API-KEY': process.env.OPENSEA_API_KEY
+                },
+                params: {
+                    owner: address,
+                    limit: 200,
+                    cursor: cursor,
+                }
+            })
+                .then((response) => __awaiter(this, void 0, void 0, function* () {
+                if (response.status == 200) {
+                    assets.push(...response.data.assets);
+                    cursor = response.data.next;
+                }
+                else {
+                    console.log("Erreur lors de la requete de récupération des assets");
+                    error = true;
+                }
+            }))
+                .catch((error) => __awaiter(this, void 0, void 0, function* () {
+                console.log("Erreur lors de la requete de récupération des assets");
+                error = true;
+            }));
+            yield new Promise(r => setTimeout(r, 4000));
+        } while (cursor && !error);
+        return assets;
+    });
+}
 //recuperation des assets pour chaque adresse
 function getWalletsAssets(walletList) {
     return __awaiter(this, void 0, void 0, function* () {
         let adressAssets = {};
         let i = 0;
         for (const address of walletList) {
-            let assets = [];
-            let cursor = '';
             i++;
-            do {
-                const openseaUrl = 'https://api.opensea.io/api/v1/assets';
-                try {
-                    axios_1.default.get(openseaUrl, {
-                        headers: {
-                            'X-API-KEY': process.env.OPENSEA_API_KEY
-                        },
-                        params: {
-                            owner: address,
-                            limit: 50,
-                            cursor: cursor,
-                        }
-                    })
-                        .then((response) => __awaiter(this, void 0, void 0, function* () {
-                        const data = yield (response === null || response === void 0 ? void 0 : response.data);
-                        //console.log('Nombre d\'asset pour ' + address + ': ' + data.assets.length)
-                        assets.push(...data.assets);
-                        cursor = data === null || data === void 0 ? void 0 : data.next;
-                    }))
-                        .catch((error) => __awaiter(this, void 0, void 0, function* () {
-                        console.log("Erreur lors de la requete de récupération des assets");
-                    }));
-                }
-                catch (e) {
-                    console.log("Erreur lors de la requete de récupération des assets");
-                }
-                yield new Promise(resolve => setTimeout(resolve, 1000));
-            } while (cursor);
-            console.log('Adresse: ' + i + '/' + walletList.length + ' - Nombre d\'asset pour ' + address + ': ' + assets.length);
-            adressAssets[address] = assets;
+            const AddAsset = yield getAdresseAsset(address);
+            console.log('Adresse: ' + i + '/' + walletList.length + ' - Nombre d\'asset pour ' + address + ': ' + AddAsset.length);
+            adressAssets[address] = AddAsset;
         }
         return adressAssets;
     });
@@ -63,7 +71,7 @@ function getWalletsAssets(walletList) {
 exports.getWalletsAssets = getWalletsAssets;
 function trackWalletsAssets(walletList) {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log('Tracking...');
+        console.log('Tracking lancé le ' + new Date().toLocaleString());
         const oldWalletFile = fs_1.default.readFileSync('./oldWallet.json', 'utf8');
         let oldWalletAsset = oldWalletFile.length > 0 ? JSON.parse(oldWalletFile) : yield getWalletsAssets(walletList);
         while (true) {
@@ -71,6 +79,7 @@ function trackWalletsAssets(walletList) {
             const walletAsset = yield getWalletsAssets(walletList);
             walletList.forEach((address) => () => __awaiter(this, void 0, void 0, function* () {
                 console.log('Nombre d\'asset pour ' + address + ': ' + walletAsset[address].length);
+                //nand pour voir les asset different entre l'ancienne liste et la nouvelle
                 let nandList = oldWalletAsset[address]
                     .filter((item) => !walletAsset[address].some((z) => z.id === item.id))
                     .concat(walletAsset[address].filter((item) => !oldWalletAsset[address].some((z) => z.id === item.id)));
@@ -79,67 +88,98 @@ function trackWalletsAssets(walletList) {
                     for (const asset of nandList) {
                         //pour chaque asset qui a changé on recupere les evenements
                         const openseaUrl = 'https://api.opensea.io/api/v1/events';
-                        try {
-                            axios_1.default.get(openseaUrl, {
+                        let error = false;
+                        let eventType = '';
+                        let cursor = null;
+                        //on boucle jusqu'a ce qu'il n'y ait plus d'erreur       
+                        do {
+                            yield new Promise(resolve => setTimeout(resolve, 4000));
+                            yield axios_1.default.get(openseaUrl, {
                                 headers: {
                                     'X-API-KEY': process.env.OPENSEA_API_KEY
                                 },
                                 params: {
                                     asset_contract_address: asset.asset_contract.address,
                                     token_id: asset.token_id,
-                                    limit: 50,
+                                    limit: 200,
+                                    cursor: cursor,
                                 },
                             })
                                 .then((response) => __awaiter(this, void 0, void 0, function* () {
-                                const data = yield (response === null || response === void 0 ? void 0 : response.data);
-                                let event = '';
-                                let y = 0;
-                                //on recupere le dernier evenement
-                                while ((event === null || event === void 0 ? void 0 : event.event_type) != 'successful' && (event === null || event === void 0 ? void 0 : event.event_type) != 'transfer') {
-                                    event = response === null || response === void 0 ? void 0 : response.data.asset_events[y];
-                                    y++;
-                                }
-                                //si c'est un achat ou une vente
-                                if (event.event_type == 'successful') {
-                                    //si l'asset est dans l'ancienne liste c'est une vente
-                                    if (asset in oldWalletAsset[address]) {
-                                        console.log('vente on adresse: ' + address + " " + asset.name + " " + asset.token_id);
-                                        (0, _1.sell)(asset);
+                                var _a;
+                                if (response.status != 200) {
+                                    const data = yield (response === null || response === void 0 ? void 0 : response.data);
+                                    let event = '';
+                                    let y = 0;
+                                    cursor = (_a = response === null || response === void 0 ? void 0 : response.data) === null || _a === void 0 ? void 0 : _a.next;
+                                    //on recupere le dernier evenement
+                                    while ((event === null || event === void 0 ? void 0 : event.event_type) != 'successful' && (event === null || event === void 0 ? void 0 : event.event_type) != 'transfer') {
+                                        event = response === null || response === void 0 ? void 0 : response.data.asset_events[y];
+                                        y++;
                                     }
-                                    //si l'asset est dans la nouvelle liste c'est un achat
-                                    else {
-                                        console.log('achat on adresse: ' + address + " " + asset.name + " " + asset.token_id);
-                                        (0, _1.buy)(asset);
-                                    }
-                                    //si c'est un transfert
-                                }
-                                else {
-                                    //si l'asset est dans l'ancienne liste c'est un envoi
-                                    if (asset in oldWalletAsset[address]) {
-                                        console.log('envoi on adresse: ' + address + " " + asset.name + " " + asset.token_id);
-                                        //si l'asset est dans la nouvelle liste c'est un asset reçu
+                                    //si c'est un achat ou une vente
+                                    if (event.event_type == 'successful') {
+                                        //si l'asset est dans l'ancienne liste c'est une vente
+                                        if (asset in oldWalletAsset[address]) {
+                                            eventType = 'sell';
+                                        }
+                                        //si l'asset est dans la nouvelle liste c'est un achat
+                                        else {
+                                            eventType = 'buy';
+                                        }
+                                        //si c'est un transfert
                                     }
                                     else {
-                                        //si il provient d'une adresse vide c'est un mint
-                                        if (event.from_account.address == '0x0000000000000000000000000000000000000000') {
-                                            console.log('mint on adresse: ' + address + " " + asset.name + " " + asset.token_id);
-                                            (0, _1.mint)(asset);
-                                            //sinon c'est un airdrop
+                                        //si l'asset est dans l'ancienne liste c'est un envoi
+                                        if (asset in oldWalletAsset[address]) {
+                                            eventType = 'send';
+                                            //si l'asset est dans la nouvelle liste c'est un asset reçu
                                         }
                                         else {
-                                            console.log('airdrop on adresse: ' + address + " " + asset.name + " " + asset.token_id);
+                                            //si il provient d'une adresse vide c'est un mint
+                                            if (event.from_account.address == '0x0000000000000000000000000000000000000000') {
+                                                eventType = 'mint';
+                                                //sinon c'est un airdrop
+                                            }
+                                            else {
+                                                eventType = 'airdrop';
+                                            }
                                         }
                                     }
+                                    eventType = event === null || event === void 0 ? void 0 : event.event_type;
+                                    error = false;
+                                }
+                                else {
+                                    console.log("Erreur lors de la requete de récupération des events");
+                                    error = true;
                                 }
                             }))
                                 .catch((error) => __awaiter(this, void 0, void 0, function* () {
                                 console.log("Erreur lors de la requete de récupération des events");
+                                error = true;
                             }));
+                        } while (error || cursor);
+                        if (eventType == 'sell') {
+                            console.log('Vente de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString());
+                            (0, _1.sell)(asset);
                         }
-                        catch (error) {
-                            console.log("Erreur lors de la requete de récupération des events");
+                        else if (eventType == 'buy') {
+                            console.log('Achat de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString());
+                            (0, _1.buy)(asset);
                         }
-                        yield new Promise(resolve => setTimeout(resolve, 1000));
+                        else if (eventType == 'mint') {
+                            console.log('Mint de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString());
+                            (0, _1.mint)(asset);
+                        }
+                        else if (eventType == 'airdrop') {
+                            console.log('Airdrop de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString());
+                        }
+                        else if (eventType == 'send') {
+                            console.log('Envoi de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString());
+                        }
+                        else {
+                            console.log('unknown event type');
+                        }
                     }
                 }
             }));
--- src/opensea.ts
+++ src/opensea.ts
@@ -4,49 +4,55 @@ import { asset } from './interface';
 import { sell, buy, mint } from '.';
 import fs from 'fs';
 
+async function getAdresseAsset(address:string){
+    const openseaUrl = 'https://api.opensea.io/api/v1/assets'
+    let assets: asset[] = []
+    let cursor = ''
+    let error = false;
+    //on boucle jusqu'a ce qu'il n'y ait plus d'erreur
+    do {
+        axios.get(openseaUrl, {
+            headers: {
+                'X-API-KEY': process.env.OPENSEA_API_KEY
+            },
+            params: {
+                owner: address,
+                limit: 200,
+                cursor: cursor,
+            }
+        })
+        .then(async (response) => {
+            if(response.status == 200){
+                assets.push(...response.data.assets)
+                cursor = response.data.next
+            }else{
+                console.log("Erreur lors de la requete de récupération des assets")
+                error = true
+            }
+        })
+        .catch(async (error) => {
+            console.log("Erreur lors de la requete de récupération des assets")
+            error = true
+        });
+        await new Promise(r => setTimeout(r, 4000));
+    } while (cursor && !error)
+    return assets
+}
+
 //recuperation des assets pour chaque adresse
 export async function getWalletsAssets(walletList: string[]) {
     let adressAssets: any = {}
     let i = 0
     for (const address of walletList) {
-        let assets: asset[] = []
-        let cursor = ''
         i++
-        do {
-            const openseaUrl = 'https://api.opensea.io/api/v1/assets'
-            try{
-                axios.get(openseaUrl, {
-                    headers: {
-                        'X-API-KEY': process.env.OPENSEA_API_KEY
-                    },
-                    params: {
-                        owner: address,
-                        limit: 50,
-                        cursor: cursor,
-                    }
-                })
-                .then(async (response) => {
-                    const data = await response?.data
-                    //console.log('Nombre d\'asset pour ' + address + ': ' + data.assets.length)
-                    assets.push(...data.assets)
-                    cursor = data?.next
-                })
-                .catch(async (error) => {
-                    console.log("Erreur lors de la requete de récupération des assets")
-                })
-            }catch(e){
-                console.log("Erreur lors de la requete de récupération des assets")
-            }
-            await new Promise(resolve => setTimeout(resolve, 1000));
-        } while (cursor)
-        console.log('Adresse: ' + i + '/' + walletList.length + ' - Nombre d\'asset pour ' + address + ': ' + assets.length)
-        adressAssets[address] = assets
+        const AddAsset = await getAdresseAsset(address)
+        console.log('Adresse: ' + i + '/' + walletList.length + ' - Nombre d\'asset pour ' + address + ': ' + AddAsset.length)
+        adressAssets[address] = AddAsset
     }
     return adressAssets
 }
-
 export async function trackWalletsAssets(walletList: string[]) {
-    console.log('Tracking...')
+    console.log('Tracking lancé le ' + new Date().toLocaleString());
     const oldWalletFile = fs.readFileSync('./oldWallet.json', 'utf8')
     let oldWalletAsset = oldWalletFile.length>0 ? JSON.parse(oldWalletFile) : await getWalletsAssets(walletList)
     while (true) {
@@ -54,6 +60,7 @@ export async function trackWalletsAssets(walletList: string[]) {
         const walletAsset = await getWalletsAssets(walletList)
         walletList.forEach((address) => async () => {
             console.log('Nombre d\'asset pour ' + address + ': ' + walletAsset[address].length)
+            //nand pour voir les asset different entre l'ancienne liste et la nouvelle
             let nandList = oldWalletAsset[address]
                 .filter((item: asset) => !walletAsset[address].some((z: asset) => z.id === item.id))
                 .concat(walletAsset[address].filter((item: asset) => !oldWalletAsset[address].some((z: asset) => z.id === item.id)));
@@ -62,65 +69,89 @@ export async function trackWalletsAssets(walletList: string[]) {
                 for (const asset of nandList) {
                     //pour chaque asset qui a changé on recupere les evenements
                     const openseaUrl = 'https://api.opensea.io/api/v1/events'
-                    try{
-                        axios.get(openseaUrl, {
-                            headers: {
-                                'X-API-KEY': process.env.OPENSEA_API_KEY
-                            },
-                            params: {
-                                asset_contract_address: asset.asset_contract.address,
-                                token_id: asset.token_id,
-                                limit: 50,
-                            },
-                        })
-                        .then(async (response) => {
-                            const data = await response?.data
-                            let event: any = ''
-                            let y = 0
-                            //on recupere le dernier evenement
-                            while (event?.event_type != 'successful' && event?.event_type != 'transfer') {
-                                event = response?.data.asset_events[y]
-                                y++;
-                            }
-                            //si c'est un achat ou une vente
-                            if (event.event_type == 'successful') {
-                                //si l'asset est dans l'ancienne liste c'est une vente
-                                if (asset in oldWalletAsset[address]) {
-                                    console.log('vente on adresse: ' + address + " " + asset.name + " " + asset.token_id)
-                                    sell(asset)
-                                }
-                                //si l'asset est dans la nouvelle liste c'est un achat
-                                else {
-
-                                    console.log('achat on adresse: ' + address + " " + asset.name + " " + asset.token_id)
-                                    buy(asset)
-                                }
-                                //si c'est un transfert
-                            } else {
-                                //si l'asset est dans l'ancienne liste c'est un envoi
-                                if (asset in oldWalletAsset[address]) {
-                                    console.log('envoi on adresse: ' + address + " " + asset.name + " " + asset.token_id)
-                                    //si l'asset est dans la nouvelle liste c'est un asset reçu
-                                } else {
-                                    //si il provient d'une adresse vide c'est un mint
-                                    if (event.from_account.address == '0x0000000000000000000000000000000000000000') {
-                                        console.log('mint on adresse: ' + address + " " + asset.name + " " + asset.token_id)
-                                        mint(asset)
-                                        //sinon c'est un airdrop
-                                    } else {
-                                        console.log('airdrop on adresse: ' + address + " " + asset.name + " " + asset.token_id)
+                    let error = false           
+                    let eventType = ''
+                    let cursor:string | null = null
+                    //on boucle jusqu'a ce qu'il n'y ait plus d'erreur       
+                    do{
+                            await new Promise(resolve => setTimeout(resolve, 4000));
+                            await axios.get(openseaUrl, {
+                                headers: {
+                                    'X-API-KEY': process.env.OPENSEA_API_KEY
+                                },
+                                params: {
+                                    asset_contract_address: asset.asset_contract.address,
+                                    token_id: asset.token_id,
+                                    limit: 200,
+                                    cursor: cursor,
+                                },
+                            })
+                            .then(async (response) => {
+                                if(response.status != 200){
+                                    const data = await response?.data
+                                    let event: any = ''
+                                    let y = 0
+                                    cursor = response?.data?.next
+                                    //on recupere le dernier evenement
+                                    while (event?.event_type != 'successful' && event?.event_type != 'transfer') {
+                                        event = response?.data.asset_events[y]
+                                        y++;
                                     }
+                                    //si c'est un achat ou une vente
+                                    if (event.event_type == 'successful') {
+                                        //si l'asset est dans l'ancienne liste c'est une vente
+                                        if (asset in oldWalletAsset[address]) {
+                                            eventType = 'sell'
+                                        }
+                                        //si l'asset est dans la nouvelle liste c'est un achat
+                                        else {
+                                            eventType = 'buy'
+                                        }
+                                        //si c'est un transfert
+                                    } else {
+                                        //si l'asset est dans l'ancienne liste c'est un envoi
+                                        if (asset in oldWalletAsset[address]) {
+                                            eventType = 'send'
+                                            //si l'asset est dans la nouvelle liste c'est un asset reçu
+                                        } else {
+                                            //si il provient d'une adresse vide c'est un mint
+                                            if (event.from_account.address == '0x0000000000000000000000000000000000000000') {
+                                                eventType = 'mint'
+                                                //sinon c'est un airdrop
+                                            } else {
+                                                eventType = 'airdrop'
+                                            }
+                                        }
+                                    }
+                                    eventType = event?.event_type
+                                    error = false
+                                }else{
+                                    console.log("Erreur lors de la requete de récupération des events")
+                                    error = true
                                 }
-                            }
+                            })
+                            .catch(async (error) => {
+                                console.log("Erreur lors de la requete de récupération des events")
+                                error = true
+                            })
 
-                        })
-                        .catch(async (error) => {
-                            console.log("Erreur lors de la requete de récupération des events")
-                        })
-                    }catch(error){
-                        console.log("Erreur lors de la requete de récupération des events")
+                    }while(error || cursor)
+                    if(eventType == 'sell'){
+                        console.log('Vente de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString())
+                        sell(asset)
+                    }else if(eventType == 'buy'){
+                        console.log('Achat de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString())
+                        buy(asset)
+                    }else if(eventType == 'mint'){
+                        console.log('Mint de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString())
+                        mint(asset)
+                    }else if(eventType == 'airdrop'){
+                        console.log('Airdrop de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString())
+                    }else if(eventType == 'send'){
+                        console.log('Envoi de ' + asset.name + ' sur l\'adresse ' + address + ' le ' + new Date().toLocaleString())
+                    }else {
+                        console.log('unknown event type')
                     }
-                    await new Promise(resolve => setTimeout(resolve, 1000));
                 }
             }
         });
